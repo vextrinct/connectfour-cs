37ca3d153b28aa9997581d1108b92689b9ca6b7d
xstraok <xstraok@proton.me>
2025-02-05 11:08:13 -0500

    i am the smartest programmer to ever exist

--- src/poopoofard/Program.cs
+++ src/poopoofard/Program.cs
@@ -7,6 +7,7 @@ namespace poopoofard
         // 8 bits per cell
         // TOO MUCH!!!!!
         // literal bloat
+        // who needs objects when you have bits
         static byte[] A = {0,0,0,1,2,1};
         static byte[] B = {0,0,0,0,0,0};
         static byte[] C = {0,0,0,0,0,0};
@@ -16,12 +17,18 @@ namespace poopoofard
         static byte[] G = {0,0,0,0,0,0};
 
         static List<byte[]> board = new List<byte[]>() {A,B,C,D,E,F,G};
+
         const byte RED = 1;
         const byte YELLOW = 2;
 
         static bool P1turn = true;
         static int cursor = 0;
 
+
+        static void Clear()
+        {
+            Console.SetCursorPosition(0, 0);
+        }
         static void WriteDash(int length)
         {
             for (int i = 0; i < length + 4; i++)
@@ -31,8 +38,8 @@ namespace poopoofard
         }
         public static void Title(string title)
         {
-            Console.Clear();
-            Console.Write("\x1b[3J");
+            Clear();
+            //Console.Write("\x1b[3J");
 
             WriteDash(title.Length);
             Console.WriteLine("\n  " + title);
@@ -79,28 +86,97 @@ namespace poopoofard
             }
             return result;
         }
-        static void Main(string[] args)
+        static void Refresh()
         {
-            ConsoleKey input =  ConsoleKey.Help;
-            while(input != ConsoleKey.Spacebar)
+            ShowHeader();
+            Console.WriteLine(StrMul(" ", cursor * 2) + (P1turn ? "X" : "O") + "            ");
+            Console.WriteLine(StrMul(" ", cursor * 2) + "V              ");
+            ShowGrid();
+        }
+        static void Round()
+        {
+            ConsoleKey input = ConsoleKey.Help;
+            while (input != ConsoleKey.Spacebar)
             {
                 ShowHeader();
                 switch (input)
                 {
                     case ConsoleKey.LeftArrow:
-                        cursor = Math.Clamp(cursor-1,0,6);
+                        cursor = Math.Clamp(cursor - 1, 0, 6);
                         break;
                     case ConsoleKey.RightArrow:
-                        cursor = Math.Clamp(cursor+1,0,6);
+                        cursor = Math.Clamp(cursor + 1, 0, 6);
                         break;
                 }
-                Console.WriteLine(StrMul(" ",cursor*2)+(P1turn ? "X" : "O"));
-                Console.WriteLine(StrMul(" ",cursor*2)+"V");
+                Console.WriteLine(StrMul(" ", cursor * 2) + (P1turn ? "X" : "O") + "            ");
+                Console.WriteLine(StrMul(" ", cursor * 2) + "V              ");
                 ShowGrid();
 
                 input = Console.ReadKey().Key;
             }
+            for (int i = 0; i < 6; i++)
+            {
+                byte coin = board[cursor][i];
+                if (coin == 0)
+                {
+                    board[cursor][i] = P1turn ? RED : YELLOW;
+                    Refresh();
+                    Thread.Sleep(50);
+                    board[cursor][i] = 0;
+                }
+                if (i==5 || coin != 0)
+                {
+                    // When I was writing this code, only I and God knew how it works.
+                    // Now only God knows it!!!!
+                    // Please do not attempt to change the following line, it will just be a waste of time
+                    // Increment the counter below as a warning for others ;)
+                    //
+                    // total_hours_wasted_here = 234;
+                    //
+                    board[cursor][i>0?coin==0?i:i-1:0]=i!=0?P1turn?RED:YELLOW:coin==(P1turn?RED:YELLOW)?P1turn?RED:YELLOW:P1turn?YELLOW:RED; // terniary operator my beloved
+                    if(i==0)
+                    {
+                        P1turn = !P1turn;
+                    }
+
+                    break;
+                }
+            }
             P1turn = !P1turn;
+        }
+
+        static bool IsWinner()
+        {
+            int coin = P1turn ? 1 : 2;
+            for (int i = 0; i < board.Count; i++)
+            {
+                int same = 0;
+                for(int j = 0; j < 6; j++)
+                {
+                    if(board[i][j] == coin)
+                    {
+                        same++;
+                        if(same>=4)
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        same = 0;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            while (!IsWinner())
+            {
+                Round();
+            }               
         }
     }
 }
